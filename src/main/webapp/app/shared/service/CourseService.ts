fca85e69448bcf3e190514c18896ac4c3faa0106
Yifeng Zhu <EthanZhu@Yifengs-MacBook-Pro.local>
2019-04-12 20:57:07 -0700

    tsetest Please enter the commit message for your changes. Lines starting

--- src/main/webapp/app/shared/service/CourseService.ts
+++ src/main/webapp/app/shared/service/CourseService.ts
@@ -11,6 +11,7 @@ export class CourseService {
     private courseAddressWithTNUrl = SERVER_API_URL + 'api/course/findAllCoursesWithTNDto';
     private courseDeleteUrl = SERVER_API_URL + 'api/course/deleteCourse';
     private courseUpdateUrl = SERVER_API_URL + 'api/course/updateCourse';
+    private addCourseUrl = SERVER_API_URL + 'api/course/addCourse';
 
     constructor(private http: HttpClient) {}
 
@@ -28,5 +29,9 @@ export class CourseService {
 
     update(course: Course): Observable<Response> {
         return this.http.put<Response>(this.courseUpdateUrl, course);
+    }
+
+    create(course: Course): Observable<Response> {
+        return this.http.post<Response>(this.addCourseUrl, course);
     }
 }
