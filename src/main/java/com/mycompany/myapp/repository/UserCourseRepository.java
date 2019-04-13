421c792262b52956b224187b4ad4990e127949e3
Yifeng Zhu <EthanZhu@Yifengs-MacBook-Pro.local>
2019-04-13 15:48:00 -0700

    test

--- src/main/java/com/mycompany/myapp/repository/UserCourseRepository.java
+++ src/main/java/com/mycompany/myapp/repository/UserCourseRepository.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 @Repository
 public interface UserCourseRepository extends JpaRepository<UserCourse, Long>{
 
-    Optional<UserCourse> findUserCourse(long course_id, long user_id);
+    // Optional<UserCourse> findUserCourse(long course_id, long user_id);
 
 }
